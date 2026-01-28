071ce890cc4fff73bcad921520156720a17c604d
mbol <matteo.bolognesi@exelen.ch>
2026-01-28 22:03:13 +0100

    Added login

---
+++ docs/firebase-config.js
@@ -0,0 +1,18 @@
+// Firebase configuration constants
+const FIREBASE_API_KEY = "AIzaSyC7u5MyVWVh8f_kSaE_q445jS5O5Qw0WPo";
+const FIREBASE_AUTH_DOMAIN = "institute-b877c.firebaseapp.com";
+const FIREBASE_PROJECT_ID = "institute-b877c";
+const FIREBASE_STORAGE_BUCKET = "institute-b877c.appspot.com";
+const FIREBASE_MESSAGING_SENDER_ID = "1035260652216";
+const FIREBASE_APP_ID = "1:1035260652216:web:a275c0bfb35ef48627224d";
+const FIREBASE_MEASUREMENT_ID = "G-98B5FH2ZEH";
+
+const firebaseConfig = {
+  apiKey: FIREBASE_API_KEY,
+  authDomain: FIREBASE_AUTH_DOMAIN,
+  projectId: FIREBASE_PROJECT_ID,
+  storageBucket: FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: FIREBASE_MESSAGING_SENDER_ID,
+  appId: FIREBASE_APP_ID,
+  measurementId: FIREBASE_MEASUREMENT_ID
+};
